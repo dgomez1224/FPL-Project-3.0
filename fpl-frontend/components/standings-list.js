663679819f6963f1c50bb146d12526937e4e97b7
David Gomez <dgomez1296@gmail.com>
2023-02-14 18:10:59 -0500

    Fixed standings loading problem

--- fpl-frontend/components/standings-list.js
+++ fpl-frontend/components/standings-list.js
@@ -5,48 +5,41 @@ import {
 } from "@/styles/Fixtures.styled";
 
 export default function StandingTable({ standings, loading }) {
-  // const findName = async (teamName) => {
-  //   let path = "/details";
-  //   const res = await fetch(process.env.NEXT_PUBLIC_API_URL + path);
-  //   const json = await res.json();
-  //   for (let entry of json.league_entries) {
-  //     if (entry.id === teamName.league_entry) {
-  //       return entry.entry_name;
-  //     }
-  //   }
-  // };
 
   if (loading) {
     return <h1>Loading...</h1>;
   }
-  return (
-    <>
-      <StyledFixtureList>
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th>Team</th>
-            <th>Wins</th>
-            <th>Draws</th>
-            <th>Losses</th>
-            <th>+</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {standings.map((fixture, index) => (
-            <StyledStandingsRow key={index}>
-              <td>{fixture.rank}</td>
-              <td>{fixture.first_name}</td>
-              <td>{fixture.matches_won}</td>
-              <td>{fixture.matches_drawn}</td>
-              <td>{fixture.matches_lost}</td>
-              <td>{fixture.points_for}</td>
-              <td>{fixture.total}</td>
-            </StyledStandingsRow>
-          ))}
-        </tbody>
-      </StyledFixtureList>
-    </>
-  );
+  else{
+    return (
+      <>
+        <StyledFixtureList>
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Team</th>
+              <th>Wins</th>
+              <th>Draws</th>
+              <th>Losses</th>
+              <th>+</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {standings.map((fixture, index) => (
+              <StyledStandingsRow key={index}>
+                <td>{fixture.rank}</td>
+                <td>{fixture.first_name}</td>
+                <td>{fixture.matches_won}</td>
+                <td>{fixture.matches_drawn}</td>
+                <td>{fixture.matches_lost}</td>
+                <td>{fixture.points_for}</td>
+                <td>{fixture.total}</td>
+              </StyledStandingsRow>
+            ))}
+          </tbody>
+        </StyledFixtureList>
+      </>
+    );
+  }
+  
 }
--- fpl-frontend/components/standings.js
+++ fpl-frontend/components/standings.js
@@ -5,57 +5,55 @@ import StandingTable from "./standings-list";
 export default function GetStandings() {
   const [loading, setLoading] = useState(true);
   const [standings, setStandings] = useState([]);
-  const [managerDetails, setManagerDetails] = useState([])
-
+  const [managerDetails, setManagerDetails] = useState([]);
   
 
-
-  
   useEffect(() => {
-   
-    const updateStandingsNames = (arr1, arr2) => {
-      for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-          if (arr1[i].league_entry === arr2[j].id) {
-            arr1[i]["first_name"] = arr2[j].player_first_name;
-          }
-        }
-      }
-      setStandings(arr1);
-    }
-  
-    const findManagerDetails = async() => {
+
+    const fetchManagerDetails = async () => {
       let path = "/details";
-      setLoading(true);
-
       const res = await fetch(process.env.NEXT_PUBLIC_API_URL + path);
       const json = await res.json();
-      setManagerDetails(json.league_entries)
-    }
+      setManagerDetails(json.league_entries);
+    };
 
-    const fetchStandings = async() => {
+    const fetchStandings = async () => {
       let path = "/standings";
-      setLoading(true);
       const res = await fetch(process.env.NEXT_PUBLIC_API_URL + path);
       const json = await res.json();
-      setStandings(json);
-      updateStandingsNames(standings, managerDetails)
+      updateStandingsNames(json);
       setLoading(false);
-    }
+    };
 
-    findManagerDetails()
-    fetchStandings()
-    console.log("standings", standings)
-    console.log("manager details", managerDetails)
-    
-  }, [])
+    const updateStandingsNames = (json) => {
+      let updatedStandings = json.map((standing) => {
+        let manager = managerDetails.find(
+          (manager) => manager.id === standing.league_entry
+        );
+        let firstName = manager ? manager.player_first_name : "";
+        return {
+          ...standing,
+          first_name: firstName,
+        };
+      });
+      setStandings(updatedStandings);
+    };
 
-  return (
-    <>
-    <h1>Standings</h1>
-    <div>
-    <StandingTable standings={standings} loading={loading} />
-  </div>
-  </>
-  );
+    fetchManagerDetails();
+    fetchStandings();
+  
+  }, [managerDetails]);
+
+
+ 
+    return (
+      <>
+        <h1>Standings</h1>
+        <div>
+          <StandingTable standings={standings}  loading={loading}/>
+        </div>
+      </>
+    );
+ 
+
 }
